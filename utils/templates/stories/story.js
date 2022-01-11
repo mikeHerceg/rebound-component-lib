9b2e73f08254aef6cf3fd72a85fe7d6f948bb420
mikeHerceg <mikehercegdesigns@gmail.com>
2022-01-11 16:10:52 -0500

    adding story template

--- utils/templates/stories/story.js
+++ utils/templates/stories/story.js
@@ -1,6 +1,25 @@
 module.exports = (kebabCaseName, paschalCaseName, titleCase) => ({
     content: `
-test story
+    import React from 'react';
+    import { ComponentStory, ComponentMeta } from '@storybook/react';
+    
+    import ${paschalCaseName} from '../../${kebabCaseName}';
+    
+    export default {
+      title: 'Components/${titleCase}',
+      component: ${paschalCaseName},
+      argTypes: {},
+    } as ComponentMeta<typeof ${paschalCaseName}>;
+    
+ 
+    const Template: ComponentStory<typeof ${paschalCaseName}> = (args) => <${paschalCaseName} {...args} />;
+    
+    export const Primary = Template.bind({});
+
+    Primary.args = {
+     
+    };
+    
   `,
     extension: `.stories.tsx`,
   });
